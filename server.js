9ddfb675513e55f66053dba95f722f25c4228e51
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2021-04-23 23:30:23 +0600

    Users Login route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
 
 
 const app = express()
+
+//bodyparser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //db config
 const db = require('./config/keys').mongoURI;
@@ -14,7 +19,6 @@ mongoose
     .connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
     .then(()=>console.log("Mongodb connection established"))
     .catch(err => console.log(err))
-
 
 app.get('/', (req, res) => res.send("hello world!"));
 
